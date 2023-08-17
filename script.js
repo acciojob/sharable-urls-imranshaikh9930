7f5bd6c80f31f59fa49a4392abd2fa7e5aa56316
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-08-17 15:18:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+
+  const button = document.getElementById("button");
+  const urlOutput = document.getElementById("url");
+
+  button.addEventListener("click", function() {
+    const name = document.getElementById("name").value;
+    const year = document.getElementById("year").value;
+
+    const queryString = `name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+    const url = `https://localhost:8080/?${queryString}`;
+    
+    urlOutput.textContent = url;
+  });
